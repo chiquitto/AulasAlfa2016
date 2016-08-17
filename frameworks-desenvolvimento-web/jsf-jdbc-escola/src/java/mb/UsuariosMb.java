7da44b9840c2381c545e2c56083965d417165d4e
Lanceloti <leandro@lanceloti.com.br>
2016-08-16 21:11:01 -0300

    Aula 16/08

---
+++ frameworks-desenvolvimento-web/jsf-jdbc-escola/src/java/mb/UsuariosMb.java
@@ -0,0 +1,26 @@
+package mb;
+
+import br.com.chiquitto.aula.jdbcescola.dao.UsuarioDao;
+import br.com.chiquitto.aula.jdbcescola.vo.Usuario;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class UsuariosMb {
+    private List<Usuario> usuarios;
+
+    public List<Usuario> getUsuarios() {
+        if (usuarios == null) {
+            UsuarioDao dao = new UsuarioDao();
+            usuarios = dao.getAll();
+        }
+        return usuarios;
+    }
+    
+    public void apagar(Usuario usuario) {
+        UsuarioDao dao = new UsuarioDao();
+        dao.apagar(usuario);
+        usuarios = null;
+    }
+    
+}
